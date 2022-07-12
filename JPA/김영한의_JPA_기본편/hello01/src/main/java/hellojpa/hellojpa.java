dbe34f9be9bb6ce98bd5ff313126f73634c24d66
hyeoKing <cksgurwkd12@naver.com>
2022-07-12 10:50:47 +0900

    FEAT: JPA의 동작 원리를 파악하기 위한 hellopJPA 자바 프로젝트

--- JPA/김영한의_JPA_기본편/hello01/src/main/java/hellojpa/hellojpa.java
+++ JPA/김영한의_JPA_기본편/hello01/src/main/java/hellojpa/hellojpa.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 /**
  * jpa 동작 원리를 보기 위한 연습용 코드
@@ -18,14 +19,23 @@ public class hellojpa {
         tx.begin();                                         // 트랜잭션 실행
 
         try {
-            Member member = new Member();                   // 멤버 받아서,
+//            Member member = new Member();                   // 멤버 받아서,
 //            member.setId(2L);                               // INSERT INTO .. ~
 //            member.setName("HelloCs");
 //            em.persist(member);                             // 앤티티에 매니저에 지금 생성한 객체를 등록해줌
 
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");         // 멤버 객체를 찾아, 이름만 수정해줘도- 커밋시에 JPA가 이를 파악해 알아서 UPDATE 해줌
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");         // 멤버 객체를 찾아, 이름만 수정해줘도- 커밋시에 JPA가 이를 파악해 알아서 UPDATE 해줌
 
+            // JPQL 예시
+            List<Member> result = em.createQuery("select m from Member as m", Member.class) // 특이한 건 대상이 테이블이 아니라 객체
+                    .setFirstResult(5) // 5번부터
+                    .setMaxResults(11) // 11개 가져와
+                    .getResultList();
+
+            for (Member member: result){
+                System.out.println("member.name = " + member.getName());
+            }
 
             tx.commit();                                    // 트랜잭션 커밋해주기
         } catch (Exception e){
