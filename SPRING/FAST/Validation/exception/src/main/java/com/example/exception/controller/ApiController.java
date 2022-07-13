9bf4191a4857f6c95c48ec204bfca79da6db986c
hyeoKing <cksgurwkd12@naver.com>
2022-07-13 14:55:19 +0900

    FEAT: Global 예외처리를 위한 예제2

--- SPRING/FAST/Validation/exception/src/main/java/com/example/exception/controller/ApiController.java
+++ SPRING/FAST/Validation/exception/src/main/java/com/example/exception/controller/ApiController.java
@@ -2,19 +2,27 @@ package com.example.exception.controller;
 
 
 import com.example.exception.dto.User;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class ApiController {
 
     @RequestMapping("")         // @RequestParam: 리퀘스트 없어도 동작하는데, 쟤는 널
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
+    public User get(
+            @Size(min = 2)
+            @RequestParam String name,
+
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age){
         User user = new User();
         user.setName(name);
         user.setAge(age);
@@ -28,8 +36,8 @@ public class ApiController {
         return user;
     }
 
-    @ExceptionHandler(value = MethodArgumentNotValidException.class) // 우선 순위는 컨트롤러의 예외처리 핸들러가 가져감 **
-    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
+//    @ExceptionHandler(value = MethodArgumentNotValidException.class) // 우선 순위는 컨트롤러의 예외처리 핸들러가 가져감 **
+//    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+//    }
 }
--- SPRING/FAST/Validation/exception/src/main/java/com/example/exception/dto/User.java
+++ SPRING/FAST/Validation/exception/src/main/java/com/example/exception/dto/User.java
@@ -7,7 +7,6 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 public class User {
-
     @NotEmpty                   // 비어있으면 안대
     @Size(min = 1, max = 10)    // 최소 한개, 최대 열개
     private String name;
@@ -16,22 +15,21 @@ public class User {
     @NotNull            // 비어있으면 안돼
     private Integer age;
 
+
+
+
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
-
     @Override
     public String toString() {
         return "User{" +
