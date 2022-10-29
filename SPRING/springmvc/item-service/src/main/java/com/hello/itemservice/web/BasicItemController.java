793d6dfcb6349a6398123e62f89a8661570d79dc
hyeoKing <cksgurwkd12@naver.com>
2022-10-30 00:31:09 +0900

    feat: item lists and tymeleaf

---
+++ SPRING/springmvc/item-service/src/main/java/com/hello/itemservice/web/BasicItemController.java
@@ -0,0 +1,52 @@
+package com.hello.itemservice.web;
+
+
+import com.hello.itemservice.domain.item.Item;
+import com.hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+//    @Autowired          // @RequiredArgsConstruct 덕분에 써주지 않아도 됨. final이 붙은 객체에 대한 생성자 주입을 대리함.
+//    public BasicItemController(ItemRepository itemRepository){
+//        this.itemRepository = itemRepository;
+//    }
+
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA", 1000, 50));
+        itemRepository.save(new Item("itemB", 4000, 1000));
+    }
+
+}
