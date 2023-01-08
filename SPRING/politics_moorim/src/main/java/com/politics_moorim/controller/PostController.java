dc7a95f96db1163b03576ad338278deb1c1622eb
hyeoKing <cksgurwkd12@naver.com>
2023-01-08 15:18:43 +0900

    feat: valid1

--- SPRING/politics_moorim/src/main/java/com/politics_moorim/controller/PostController.java
+++ SPRING/politics_moorim/src/main/java/com/politics_moorim/controller/PostController.java
@@ -1,18 +1,50 @@
 package com.politics_moorim.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.politics_moorim.request.RoarCreate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/roar")
+@Slf4j
 public class PostController {
+    // HTTP Method
+    // GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT
 
-    @GetMapping("")
+    @GetMapping("/testcall")
     public String testGet(){
         return "Hi, hyeok i'm your first commercial project";
     }
 
+    @PostMapping("")
+    public Map<String, String> roarToWorld(@RequestBody @Valid RoarCreate param, BindingResult result){
+        // 데이터 검증의 이유
+        // 1. client 개발자가 깜빡할 수 있다. (휴먼 에러)
+        // 2. client bug로 값이 누락될 수 있다.
+        // 3. 외부에 나쁜 사람이 값을 임의로 조작해서 보낼 수 있다.
+        // 4. DB에 값을 저장할 때 의도치 않은 오류가 발생할 수 있다.
+        // 5. 서버 개발자의 편안함을 위해서
 
+        if (result.hasErrors()) {
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            FieldError firestFieldError = fieldErrors.get(0);
+
+            String invalidFieldName = firestFieldError.getField();       // title
+            String errorMessage = firestFieldError.getDefaultMessage();  // error message
+
+            Map<String, String> error = new HashMap<>();
+            error.put(invalidFieldName, errorMessage);
+            return error;
+        }
+
+        return Map.of();
+    }
 
 }
--- SPRING/politics_moorim/src/test/java/com/politics_moorim/controller/PostControllerTest.java
+++ SPRING/politics_moorim/src/test/java/com/politics_moorim/controller/PostControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -15,11 +16,37 @@ class PostControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("테스트용 호출을 테스트 (설명에 수미상관법 적용)")
+    @DisplayName("/roar로 post 요청시 사용자의 포효(게시글) 저장")
+    void roarTest() throws Exception {
+
+        mockMvc.perform(post("/roar")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"title\": \"제목입니다.\", \"content:\":  \"내용입니다.\"}")
+                        )
+                .andExpect(status().isOk())
+                .andExpect(content().string("success"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("/roar로 post 요청시 title 값을 필수 !")
+    void roarTestVaild() throws Exception {
+
+        mockMvc.perform(post("/roar")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": null, \"content:\":  \"내용입니다.\"}")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("타이틀을 입력해주세요."))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("테스트용 get request 호출을 테스트 (설명에 수미상관법 적용)")
     void testGetTest() throws Exception {
         String expectedWord = "Hi, hyeok i'm your first commercial project";
         // expected
-        mockMvc.perform(get("/test"))
+        mockMvc.perform(get("/roar/testcall"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hi, hyeok i'm your first commercial project"))
                 .andDo(print());
