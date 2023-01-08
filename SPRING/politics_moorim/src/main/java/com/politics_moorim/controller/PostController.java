16b694b82f249c7cf97c389668890c92473d4dca
hyeoKing <cksgurwkd12@naver.com>
2023-01-08 21:23:14 +0900

    feat: valid using adviceController

--- SPRING/politics_moorim/src/main/java/com/politics_moorim/controller/PostController.java
+++ SPRING/politics_moorim/src/main/java/com/politics_moorim/controller/PostController.java
@@ -24,7 +24,7 @@ public class PostController {
     }
 
     @PostMapping("")
-    public Map<String, String> roarToWorld(@RequestBody @Valid RoarCreate param, BindingResult result){
+    public Map<String, String> roarToWorld(@RequestBody @Valid RoarCreate param){
         // 데이터 검증의 이유
         // 1. client 개발자가 깜빡할 수 있다. (휴먼 에러)
         // 2. client bug로 값이 누락될 수 있다.
@@ -32,17 +32,20 @@ public class PostController {
         // 4. DB에 값을 저장할 때 의도치 않은 오류가 발생할 수 있다.
         // 5. 서버 개발자의 편안함을 위해서
 
-        if (result.hasErrors()) {
-            List<FieldError> fieldErrors = result.getFieldErrors();
-            FieldError firestFieldError = fieldErrors.get(0);
 
-            String invalidFieldName = firestFieldError.getField();       // title
-            String errorMessage = firestFieldError.getDefaultMessage();  // error message
 
-            Map<String, String> error = new HashMap<>();
-            error.put(invalidFieldName, errorMessage);
-            return error;
-        }
+//            // add ( ,BindingResult result) parameter
+//        if (result.hasErrors()) {
+//            List<FieldError> fieldErrors = result.getFieldErrors();
+//            FieldError firestFieldError = fieldErrors.get(0);
+//
+//            String invalidFieldName = firestFieldError.getField();       // title
+//            String errorMessage = firestFieldError.getDefaultMessage();  // error message
+//
+//            Map<String, String> error = new HashMap<>();
+//            error.put(invalidFieldName, errorMessage);
+//            return error;
+//        }
 
         return Map.of();
     }
