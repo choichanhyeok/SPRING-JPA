98292098344e6f65e78ba27d3d4d50e6eda35c1f
hyeok <cksgurwkd12@naver.com>
2023-03-27 00:14:55 +0900

    feat: 타임 리프에서의 문자escape 해결

--- SPRING/spring_mvc_2/thymeleaf-basic/src/main/java/com/hello/thymeleaf/basic/BasicController.java
+++ SPRING/spring_mvc_2/thymeleaf-basic/src/main/java/com/hello/thymeleaf/basic/BasicController.java
@@ -11,7 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BasicController {
     @GetMapping("text-basic")
     public String textBasic(Model model){
-        model.addAttribute("data", "Hello Spring!");
+        model.addAttribute("data", "<b>Hello </b>Spring!");
         return "basic/text-basic";
+    }
+
+    @GetMapping("text-unescaped")
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "Hello <b>Spirng! </b>");
+        return "basic/text-unescaped";
     }
 }
